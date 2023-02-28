b0e3377ceb361197cad060c017312f87e54481cf
Latif Turaan <latiftoran@gmail.com>
2023-02-28 17:37:05 +0100

    the create and new actions are created and a form is created in view

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -3,4 +3,26 @@ class ToolsController < ApplicationController
   def index
     @tools = Tool.all
   end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    @tool.user = current_user
+    if @tool.save
+      redirect_to tool_path(@tool)
+      flash.now[:success] = "The Tool has been created"
+    else
+      flash.now[:error] = "Your data is not saved. Please provide valid data and try again."
+      render :new
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :price)
+  end
 end
