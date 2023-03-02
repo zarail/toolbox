a80f884a1617d584de3095c8eb7032f92118f0f3
Zahra Ilkhan <zahraa1991@gmail.com>
2023-03-02 15:17:36 +0100

    add blocked dates, finished calendar

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -5,9 +5,15 @@ import flatpickr from "flatpickr";
 export default class extends Controller {
   // Inform the controller that it has two targets in the form, which are our inputs
   static targets = [ "startTime", "endTime" ]
+  static values = {
+    booked: Array
+  }
 
   connect() {
-    flatpickr(this.startTimeTarget, {})
+    flatpickr(this.startTimeTarget, {
+      disable: this.bookedValue.map((date) => Date.parse(date)),
+    })
+
     flatpickr(this.endTimeTarget, {})
   }
 }
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -4,4 +4,10 @@ class Tool < ApplicationRecord
 
   validates :name, :price, presence: true
   validates :price, numericality: true
+
+  def booked_dates
+    bookings.map do |booking|
+      (booking.start_date..booking.end_date).map { |date| date.strftime("%Y-%m-%d") }
+    end.flatten
+  end
 end
